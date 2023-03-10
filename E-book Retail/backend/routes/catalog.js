62c1ced2e50fde6ad3cb6c9c8fab34278c6cb47a
VuDoiMu <98081088+VuDoiMu@users.noreply.github.com>
2023-03-11 01:52:29 +0700

    Delete E-book Retail/backend directory

--- E-book Retail/backend/routes/catalog.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-//TODO: handle catalog side management
-
-
-module.exports = router
--- E-book Retail/backend/routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-//TODO: handle user side management
-
-
-module.exports = router
