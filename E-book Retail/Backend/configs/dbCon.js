15f93dca8a4b3353532e5af9fee8f455d4c1d8e2
Kien <kienvu1410@gmail.com>
2023-03-15 21:43:52 +0700

    Update view

--- E-book Retail/Backend/configs/dbCon.js
+++ E-book Retail/Backend/configs/dbCon.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/E-bookRetailer' , {
-          useUnifiedTopology: true,
-          useNewUrlParser: true  
-        })
-    } catch (err) {
-        console.error(err);
-    }
-}
+  try {
+    await mongoose.connect(
+      "mongodb+srv://root:root@cluster0.ujgarwx.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = connectDB;
--- E-book Retail/Backend/server.js
+++ E-book Retail/Backend/server.js
@@ -1,10 +1,9 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const connectDB = require('./configs/dbCon');
-const path = require('path');
-const data = require('./data/book.json');
+const mongoose = require("mongoose");
+const connectDB = require("./configs/dbCon");
+const path = require("path");
+const data = require("./data/book.json");
 //connect to MongoDb
 connectDB();
 
@@ -23,40 +22,51 @@ app.set("views", path.join(__dirname, "views"));
 
 // routes for render pug files
 app.get("/", (req, res) => {
-    console.log(data);
-    res.render("home.pug", {
-      data,
-    });
+  res.render("home.pug", {
+    data,
   });
-  app.get("/gio-hang", (req, res) => {
-    res.render("gio-hang.pug", {
-      title: "hello world",
-    });
+});
+app.get("/gio-hang", (req, res) => {
+  res.render("gio-hang.pug", {
+    title: "hello world",
   });
-  app.get("/product-list", (req, res) => {
-    res.render("product-list.pug", {
-      title: "hello world",
-    });
+});
+app.get("/product-list", (req, res) => {
+  res.render("product-list.pug", {
+    title: "hello world",
   });
-  
-   app.get("/product/:id", (req, res) => {
-    const id = req.params.id;
-    res.render("product-list.pug", {
-      title: "hello world",
+});
+
+app.get("/product/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  let book = null;
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i]._id.$oid === id) {
+      book = data[i];
+    }
+    res.render("product.pug", {
+      book,
     });
+  }
+
+  res.render("product.pug", {
+    title: "hello world",
   });
-  
-  app.get("/tai-khoan", (req, res) => {
-    res.render("tai-khoan.pug", {
-      title: "hello world",
-    });
+});
+
+app.get("/tai-khoan", (req, res) => {
+  res.render("tai-khoan.pug", {
+    title: "hello world",
   });
+});
 
 //routes
-app.use('/management/', require('./routes/books'));
-app.use('/order', require('./routes/order'));
+app.use("/management/", require("./routes/books"));
+app.use("/order", require("./routes/order"));
 
-mongoose.connection.once('open', () => {
-    console.log('connected to MongoDb');
-    app.listen(3500, () => console.log('Server running on port 3500!'));
+mongoose.connection.once("open", () => {
+  console.log("connected to MongoDb");
+  app.listen(3500, () => console.log("Server running on port 3500!"));
 });
