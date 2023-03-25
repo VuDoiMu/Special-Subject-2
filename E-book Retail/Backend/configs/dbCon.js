d40f8db43b8ff9067503db21e68d5c8d2af8d806
VuDoiMu <trinhlongvu153@gmail.com>
2023-03-25 14:37:23 +0700

    dbCon changes

--- E-book Retail/Backend/configs/dbCon.js
+++ E-book Retail/Backend/configs/dbCon.js
@@ -4,8 +4,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect(
       "mongodb+srv://admin:pass123@unistuf.nze6bno.mongodb.net/Emanga",
-      "mongodb+srv://admin:pass123@unistuf.nze6bno.mongodb.net/Emanga",
-      // "mongodb+srv://root:root@cluster0.ujgarwx.mongodb.net/?retryWrites=true&w=majority",
       {
         useUnifiedTopology: true,
         useNewUrlParser: true,
