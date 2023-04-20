54998ecb1574ec091b32ce55da665ac09690fdcf
Kien <kienvu1410@gmail.com>
2023-04-20 20:29:51 +0700

    fix create book

--- E-book Retail/Backend/public/js/create-book.js
+++ E-book Retail/Backend/public/js/create-book.js
@@ -12,7 +12,7 @@ async function uploadImages(event) {
   // create form data object from <form>
   const formData = new FormData(form);
   console.log(formData.get("images"));
-  fetch("http://localhost:3500/uploadAvatar", {
+  fetch("http://localhost:3500/management", {
     method: "POST",
     body: formData,
   })
